12f372232889d292dcdf5c9ef6e558c56e7fab3a
unknown <sama.shoummo@gmail.com>
2020-03-30 01:32:10 +0600

    pure css experiments

---
+++ Image_with_pure_CSS/pure_CSS_image_generator.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+image = cv2.imread("a.jpeg")
+ImageSize = image.shape
+
+height = ImageSize[0]
+width = ImageSize[1]
+
+print(height)
+print(width)
+
+f= open("generator.html","w+")
+
+f.write("<!DOCTYPE html>\n")
+f.write("<html>\n")
+f.write("<title>CSS generator by python</title>\n")
+f.write("</head>\n")
+
+count=0
+f.write("<div style=\"width: 100%; display: table;\">")
+f.write("<div style=\"display: table-row; height: 1px;\">")
+
+for x in range (0,height):
+    for y in range (0,width):
+    	count=count+1
+    	pixel = image[x,y]
+    	f.write("<div style=\"display:table-cell;background-color:rgba("+str(pixel[0])+","+str(pixel[1])+","+str(pixel[2])+");\"></div>\n")
+    	if count%width==0:
+    		f.write("</div>")
+    		f.write("</div>")
+    		f.write("<div style=\"width: 100%; display: table;\">")
+    		f.write("<div style=\"display: table-row; height: 1px;\">")
+#f.write("<div style=\"float:left;width: 1px;height: 1px;background-color:rgba("+str(pixel[0])+","+str(pixel[1])+","+str(pixel[2])+");\"></div>\n")
+f.write("</body>\n")
+f.write("</html>\n")
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Crazy_python_experiments
+
+Doing some crazy python experiments with weird projects..Mostly automation based projects on weird project themseleves
